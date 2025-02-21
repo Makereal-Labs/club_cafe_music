66c4900addd84e48b810d9ba706738b7b1f6ad25
Makereal Labs <makereallabsnctu@gmail.com>
2025-02-21 22:35:57 +0800

    create a queue for sources

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,31 @@
-use rodio::{Decoder, Sink};
+use rodio::{Decoder, Sink, source::Source};
+use std::collections::VecDeque;
+use std::mem::forget;
 use std::net::{TcpListener, TcpStream};
+use std::thread::sleep;
+use std::time::Duration;
 use tungstenite::accept;
 
 /// A WebSocket echo server
 fn main() {
-    let (_stream, stream_handle) = rodio::OutputStream::try_default().unwrap();
+    let (stream, stream_handle) = rodio::OutputStream::try_default().unwrap();
+    forget(stream);
     let sink = Sink::try_new(&stream_handle).unwrap();
+    let mut queue: VecDeque<Box<dyn Source<Item = f32> + Send>> = VecDeque::new();
+
     let file = std::io::BufReader::new(std::fs::File::open("/home/makereal/forever.mp3").unwrap());
     let source = Decoder::new(file).unwrap();
-    sink.append(source);
+    queue.push_back(Box::new(source.convert_samples()));
 
     let server = TcpListener::bind("0.0.0.0:9001").unwrap();
     std::thread::scope(|s| {
+        s.spawn(|| {
+            sink.sleep_until_end();
+            sleep(Duration::from_millis(200));
+            if let Some(source) = queue.pop_front() {
+                sink.append(source);
+            }
+        });
         for stream in server.incoming() {
             match stream {
                 Ok(stream) => {
