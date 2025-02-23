acbafa50267fd2d66708b5bccc8b5b965c065f0f
Quick <quick0831@gmail.com>
2025-02-23 23:22:09 +0800

    rename theme.tsx to theme.ts

--- src/main.tsx
+++ src/main.tsx
@@ -2,7 +2,7 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { ThemeProvider } from '@mui/material'
 import App from './App.tsx'
-import theme from './theme.tsx'
+import theme from './theme.ts'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
