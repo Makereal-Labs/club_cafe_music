3918b2d339ec96eee8aa6da8d8f16e9b6ba41911
Quick <quick0831@gmail.com>
2025-03-03 17:25:55 +0800

    add state to time slider

--- src/Player.tsx
+++ src/Player.tsx
@@ -16,12 +16,22 @@ import {
 } from "@mui/material";
 import { useState } from "react";
 
+function format_time(time: number): String {
+  return `${Math.floor(time / 60)}:` + `${Math.floor(time) % 60}`.padStart(2, "0");
+}
+
 function Player() {
   const theme = useTheme();
   const [playing, setPlaying] = useState(false);
+  const [play_time, setPlayTime] = useState(0);
+  const total_time = 180;
 
-  const current_time = "1:23";
-  const remaining_time = "4:56";
+  const current_time = format_time(play_time);
+  const remaining_time = format_time(total_time - play_time);
+
+  function on_playtime_slider_change(time: number) {
+    setPlayTime(time);
+  }
 
   return (
     <Box sx={{
@@ -31,7 +41,15 @@ function Player() {
       borderRadius: "4px",
       backgroundColor: theme.palette.background.paper,
     }}>
-      <Slider size="small" sx={{ color: "white" }} />
+      <Slider
+        size="small"
+        sx={{ color: "white" }}
+        min={0}
+        step={1}
+        max={total_time}
+        value={play_time}
+        onChange={(_, value) => on_playtime_slider_change(value as number)}
+      />
       <Box display="flex" justifyContent="space-between">
         <Typography variant="overline">{current_time}</Typography>
         <Typography variant="overline">-{remaining_time}</Typography>
