258c37e5c60ae9e6fbb4524ecee86938b04507af
Quick <quick0831@gmail.com>
2025-05-05 21:16:12 +0800

    put the whole context in async

--- src/main.rs
+++ src/main.rs
@@ -8,7 +8,7 @@ mod yt_dlp;
 use std::collections::VecDeque;
 
 use smol::prelude::*;
-use smol::{Executor, block_on, channel, lock::Mutex, net::TcpListener};
+use smol::{Executor, block_on, channel, future::zip, lock::Mutex, net::TcpListener};
 
 use handler::handle;
 use player::player;
@@ -30,44 +30,35 @@ fn main() {
 
     let server = block_on(TcpListener::bind("0.0.0.0:9001")).unwrap();
 
-    std::thread::scope(|s| {
-        s.spawn(move || {
-            block_on(player(state, broadcast_tx));
-        });
-        let ref_event_listeners = &event_listeners;
-        s.spawn(|| {
-            let ex = Executor::new();
-            block_on(ex.run(async {
-                let mut incoming = server.incoming();
-                while let Some(stream) = incoming.next().await {
-                    match stream {
-                        Ok(stream) => {
-                            let (tx, rx) = channel::unbounded();
-                            let _ = tx.send(Event).await;
-                            ref_event_listeners.lock().await.push(tx);
-                            ex.spawn(async move {
-                                if let Err(error) = handle(stream, state, rx).await {
-                                    eprintln!("Error while handling socket: {error}");
-                                }
-                            })
-                            .detach();
+    let ex = Executor::new();
+    let task = player(state, broadcast_tx);
+    let task = zip(task, async {
+        let mut incoming = server.incoming();
+        while let Some(stream) = incoming.next().await {
+            match stream {
+                Ok(stream) => {
+                    let (tx, rx) = channel::unbounded();
+                    let _ = tx.send(Event).await;
+                    event_listeners.lock().await.push(tx);
+                    ex.spawn(async move {
+                        if let Err(error) = handle(stream, state, rx).await {
+                            eprintln!("Error while handling socket: {error}");
                         }
-                        Err(error) => {
-                            eprintln!("Error listening socket: {error}");
-                        }
-                    }
+                    })
+                    .detach();
                 }
-            }));
-        });
-        let ref_event_listeners = &event_listeners;
-        s.spawn(move || {
-            block_on(async {
-                while let Ok(event) = broadcast_rx.recv().await {
-                    for listener in ref_event_listeners.lock().await.iter() {
-                        let _ = listener.send(event).await;
-                    }
+                Err(error) => {
+                    eprintln!("Error listening socket: {error}");
                 }
-            });
-        });
+            }
+        }
     });
+    let task = zip(task, async {
+        while let Ok(event) = broadcast_rx.recv().await {
+            for listener in event_listeners.lock().await.iter() {
+                let _ = listener.send(event).await;
+            }
+        }
+    });
+    block_on(ex.run(task));
 }
